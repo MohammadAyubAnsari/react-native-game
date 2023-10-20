99a4f47edb84be0630e6a0830b8a7bdf256bec83
Mohammad Ayub Ansari <119043948+MohammadAyubAnsari@users.noreply.github.com>
2023-10-20 20:21:47 +0530

    adding and using custom fonts with react native

--- App.js
+++ App.js
@@ -5,10 +5,20 @@ import { useState } from "react";
 import GameScreen from "./screens/GameScreen";
 import Colors from "./constatnts/colors";
 import GameOverScreen from "./screens/GameOverScreen";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState();
+
+  const [fontsLaoded] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+  if (!fontsLaoded) {
+    return <AppLoading />;
+  }
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
